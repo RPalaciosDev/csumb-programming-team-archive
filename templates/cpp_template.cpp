081aa776d0d058e9e1e2080e3e7b9176c75f25ad
Roberto Palacios <rpalacios@csumb.edu>
2025-07-21 21:51:41 -0700

    Inaugural commit for the archive with file structure and templates (#1)
    Self merging for now

---
+++ templates/cpp_template.cpp
@@ -0,0 +1,31 @@
+/**
+* ===========================================================
+*     Author:        Jane Doe
+*     Created:       20XX-XX-XX
+*     Last Updated:  2025-XX-XX
+*     Problem:       0001 - Two Sum
+*     Platform:      LeetCode
+*     Difficulty:    Easy
+*     Language:      C++
+*     Tags:          
+* ===========================================================
+*
+* Description:
+* --------------------
+* XXXXX
+* 
+* Approach:
+* ___________________
+* XXXXX
+*/
+
+# include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+int main() {
+
+    }
---
+++ templates/python_template.py
@@ -0,0 +1,23 @@
+"""
+===========================================================
+    Author:        Jane Doe
+    Created:       20XX-XX-XX
+    Last Updated:  2025-XX-XX
+    Problem:       0001 - Two Sum
+    Platform:      LeetCode
+    Difficulty:    Easy
+    Language:      Python 3
+    Tags:          
+===========================================================
+
+Description:
+--------------------
+XXXXX
+
+Approach:
+___________________
+XXXXX
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
