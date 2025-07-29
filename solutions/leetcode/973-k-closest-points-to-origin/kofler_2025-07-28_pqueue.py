aa8cc61af02e87d9f50559ee1c5dcd7f1022411f
Edwin Kofler <edwin@kofler.dev>
2025-07-28 19:48:32 -0700

    Add comments

--- solutions/leetcode/973-k-closest-points-to-origin/kofler_2025-07-28_pqueue.py
+++ solutions/leetcode/973-k-closest-points-to-origin/kofler_2025-07-28_pqueue.py
@@ -1,3 +1,24 @@
+"""
+LINK: https://leetcode.com/problems/k-closest-points-to-origin/submissions/1715199479
+
+APPROACH:
+(1) Make a list, which will be used as a heap queue to store distances of
+    points to the origin
+(2) Loop through `points`: for each point, calculate its distance and append
+    the distance-point pair to the array as a tuple.
+(3) "heapify" the list. This is faster than using heapq to incrementally add
+    new pairs
+(3) Pick and return first k points in the list, which are the k closest points
+    to the origin.
+
+NOTES:
+Time Complexity:
+- Linear
+
+Space Complexity:
+- Linear
+"""
+
 import heapq
 from typing import Tuple
 
@@ -12,5 +33,6 @@ class Solution:
         answer: list[list[int]] = []
         for _ in range(k):
             _, point = heapq.heappop(nearest_points)
-            answer.append(list(point)) # Use list() to satisfy type checker.
+            # Use list() to satisfy type checker at the cost of performance.
+            answer.append(list(point))
         return answer
